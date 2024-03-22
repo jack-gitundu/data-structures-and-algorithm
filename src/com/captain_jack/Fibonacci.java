f801bb42d0903a18ea8c9ee72b9a8197686850f0
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-03-22 10:40:23 +0300

    fibonacci sequence using dynamic programming-memoization

---
+++ src/com/captain_jack/Fibonacci.java
@@ -0,0 +1,35 @@
+package com.captain_jack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Write a function fib(n) that takes in a number as an argument.
+ * The function should return the n-th number of Fibonacci sequence
+ */
+public class Fibonacci {
+
+    static Map<Long, Long> memo = new HashMap<>();
+
+    public static void main(String[] args) {
+        Long n = 50L;
+        System.out.println("\n===================>");
+        System.out.println("The " + n + "th Fibonacci number is: " + fib(n));
+        System.out.println("===================>");
+
+    }
+
+    public static Long fib(Long n) {
+        if (n <= 1) return 1L;
+
+        // Check if result for 'n' is already computed and stored in the memo
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        // If not, compute and store the result in the memo
+        Long fibResult = fib(n - 1) + fib(n - 2);
+        memo.put(n, fibResult);
+        return fibResult;
+    }
+}
