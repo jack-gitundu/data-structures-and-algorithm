12f4a83a75d3d63d6db655d62190656907a1bc4c
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-03-25 17:26:19 +0300

    count Number of Duplicate Words in a Given String

--- src/com/captain_jack/DuplicateWordsInString.java
+++ src/com/captain_jack/DuplicateWordsInString.java
@@ -1,2 +1,44 @@
-package com.captain_jack;public class DuplicateWordsInString {
+package com.captain_jack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Java Program to Count Number of Duplicate Words in a Given String
+ */
+public class DuplicateWordsInString {
+
+    public static void main(String[] args) {
+
+        duplicateWords("java guides java");
+
+        duplicateWords("Java is java again java");
+
+        duplicateWords("Super Man Bat Man Spider Man");
+    }
+
+    private static void duplicateWords(String inputString) {
+        // Splitting inputString into words
+        final String[] words = inputString.toLowerCase().split("\\W+");
+
+        // Creating one HashMap with words as key and their count as value
+        final Map<String, Integer> wordCount = new HashMap<>();
+
+        // Checking each word
+        for (String word : words) {
+            // Incrementing the count of the word
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> word : wordCount.entrySet()) {
+
+            if (word.getValue() > 1) {
+                // Printing duplicate word and its count
+                System.out.println(word.getKey() + " : " + word.getValue());
+            }
+        }
+    }
 }
+
+
--- src/com/captain_jack/searching_algorithms/BinarySearchIteratively.java
+++ src/com/captain_jack/searching_algorithms/BinarySearchIteratively.java
@@ -1,4 +1,4 @@
-package com.captain_jack; /**
+package com.captain_jack.searching_algorithms; /**
  * Binary search is a fast search algorithm with run-time complexity of Ο(log n).
  * This search algorithm works on the principle of divide and conquer.
  * For this algorithm to work properly, the data collection should be in the sorted form.
