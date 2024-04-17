e294e13cf1b7e7ec49e39c6d8d057b916d1e2dd3
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-04-17 13:02:33 +0300

    add explanation

--- src/com/captain_jack/Gcd.java
+++ src/com/captain_jack/Gcd.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 /**
  * Given two numbers, calculate the Greatest Common Divisor(GCD) or the Highest Common Factor(HCF
+ * <p>
+ * Euclid's algorithm
+ * 1. Find the largest number.
+ * 2. Replace the larger number by the difference between the larger and the smaller number.
+ * 3. Repeat step 1 & 2 until both numbers become equal.
+ * 4. Return any one number, which is the GCd.
  */
 public class Gcd {
     public static void main(String[] args) {
