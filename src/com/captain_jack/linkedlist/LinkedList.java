b2df63ceef8645ceefa80c378c0aea7f44e323ca
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-09-03 09:53:16 +0300

    create a linked list, print list and append value

---
+++ src/com/captain_jack/linkedlist/LinkedList.java
@@ -0,0 +1,55 @@
+package com.captain_jack.linkedlist;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
---
+++ src/com/captain_jack/linkedlist/Main.java
@@ -0,0 +1,12 @@
+package com.captain_jack.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.append(1);
+
+        myLinkedList.printList();
+    }
+}
