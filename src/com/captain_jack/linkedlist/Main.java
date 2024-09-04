58c0c3bd9a70c9ccd7c9ebbcbe0b6bcdfb2d4d99
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-09-04 11:35:39 +0300

    remove the last

--- src/com/captain_jack/linkedlist/Main.java
+++ src/com/captain_jack/linkedlist/Main.java
@@ -3,10 +3,18 @@ package com.captain_jack.linkedlist;
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(4);
-
-        myLinkedList.append(1);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
 
         myLinkedList.printList();
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeLast().value);
+
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeLast());
+
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeLast());
     }
 }
