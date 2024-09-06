ff842dbe253cabe7427e04a11908ce261fa6b353
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-09-06 11:44:32 +0300

    prepend a value to the linkedList

--- src/com/captain_jack/linkedlist/Main.java
+++ src/com/captain_jack/linkedlist/Main.java
@@ -3,18 +3,20 @@ package com.captain_jack.linkedlist;
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(3);
+
+        myLinkedList.prepend(1);
 
         myLinkedList.printList();
 
         // (2) Items - Returns 2 Node
-        System.out.println(myLinkedList.removeLast().value);
+//        System.out.println(myLinkedList.removeLast().value);
 
         // (1) Item - Returns 1 Node
-        System.out.println(myLinkedList.removeLast());
+//        System.out.println(myLinkedList.removeLast());
 
         // (0) Items - Returns null
-        System.out.println(myLinkedList.removeLast());
+//        System.out.println(myLinkedList.removeLast());
     }
 }
