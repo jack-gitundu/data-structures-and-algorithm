45f1116960850ed91d5764f94da7520164630208
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2025-05-23 16:06:51 +0300

    add proper problem description

--- src/com/captain_jack/seco_largest_element/SecondLargestElementNaive.java
+++ src/com/captain_jack/seco_largest_element/SecondLargestElementNaive.java
@@ -10,6 +10,20 @@ import java.util.Arrays;
  * Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
  * <p>
  * Note: If the second largest element does not exist, return -1.
+ * <p>
+ * Examples:
+ * <p>
+ * Input: arr[] = [12, 35, 1, 10, 34, 1]
+ * Output: 34
+ * Explanation: The largest element of the array is 35 and the second largest element is 34.
+ * <p>
+ * Input: arr[] = [10, 5, 10]
+ * Output: 5
+ * Explanation: The largest element of the array is 10 and the second largest element is 5.
+ * <p>
+ * Input: arr[] = [10, 10, 10]
+ * Output: -1
+ * Explanation: The largest element of the array is 10 there is no second largest element.
  */
 public class SecondLargestElementNaive {
 
